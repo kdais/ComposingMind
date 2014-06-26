29362a68ca6046585becbfd832898db714d0b14b
Maryan Pritsak <maryan.pricak@gmail.com>
2014-06-27 02:37:12 +0300

    Added ScalaTest framework.

---
+++ HTM Research Project/src/Test/DendriteSegmentSuite.scala
@@ -0,0 +1,25 @@
+package Test
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import Core.DendriteSegment
+
+@RunWith(classOf[JUnitRunner])
+class DendriteSegmentSuite extends FunSuite {
+  
+  trait TestSegments {
+    val (length, boost, threshold, radius) = (10, 3, 0.5F, 0.3F)
+    val indexes = List(1, 4, 6, 8, 12, 22, 25, 36, 40, 44)
+    val permanences = DendriteSegment.genPermanences(length, threshold, radius)
+    val segment = new DendriteSegment(indexes.zip(permanences), threshold, boost)
+  }
+  
+  test ("DendriteSegment : Random permanences within radius") {
+    new TestSegments {
+      assert(permanences.forall(p => p >= threshold - radius && p <= threshold + radius) === true)
+      assert(permanences.exists(p => p != threshold) === true)
+    }
+  }
+}
