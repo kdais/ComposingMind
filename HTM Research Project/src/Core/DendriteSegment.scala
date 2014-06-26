f55a69fc88de801b98266f6bb4c7fd1e6afc3906
Maryan Pritsak <maryan.pricak@gmail.com>
2014-06-27 00:13:24 +0300

    Changed comments.

--- HTM Research Project/src/Core/DendriteSegment.scala
+++ HTM Research Project/src/Core/DendriteSegment.scala
@@ -62,9 +62,9 @@ class DendriteSegment(private val m_connections : List[(Int, Float)], // (connec
 		   
   /**
    * Calculates overlap of the segment over given data. 
-   * @param data collection of inputs to the segment
-   * @param get function that gets value from data at specified index
-   * @return overlap value over input data multiplied by boost
+   * @param data collection of inputs to the segment.
+   * @param get function that gets value from data at specified index.
+   * @return overlap value over input data multiplied by boost.
    */
   def overlap[T](data : Vector[T], get : T => Int) : Int = {
     // Choose connections with permanence higher than threshold.
