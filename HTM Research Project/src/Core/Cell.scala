e794365288bf6ad48020e65d2089134760987bb6
Maryan Pritsak <maryan.pricak@gmail.com>
2014-07-09 04:09:37 +0300

    Implemented basic functionality for Cell class.

--- HTM Research Project/src/Core/Cell.scala
+++ HTM Research Project/src/Core/Cell.scala
@@ -2,39 +2,89 @@ package Core
 
 /**
  * @brief class Cell represents a single cell in a column that has its unique behavior
- * in terms of column's input and prediction of distal dendrites.   
+ * in terms of column's input and prediction of distal dendrites.
+ * @param m_steps size of history and "prediction".
+ * @param m_stateHistory history of previous states - active or inactive.
+ * @param m_distalSegments vector of distal segments for making prediction.
  */
-class Cell(private val m_state : Int) {
+class Cell(private val m_steps : Int,
+		   private val m_stateHistory : Vector[Boolean],
+		   private val m_distalSegments : Vector[List[DistalSegment]]) {
+  
+  require(m_steps > 0 && m_stateHistory.length == m_steps && m_distalSegments.length == m_steps)
   
   /**
-   * Checks whether cell is in active state.
-   * @return true if cell is in active state.
+   * Makes an empty cell with history size set to steps.
+   * @param steps size of history and "prediction".
+   * @return empty cell.
    */
-  def isActive() : Boolean = (m_state & Cell.Active) != 0
+  def this(steps : Int) = this(steps, Vector.fill(steps)(false), Vector.fill(steps)(Nil))
   
   /**
-   * Checks whether cell is in predictive state.
-   * @return true if cell is in predictive state.
+   * Makes cell active by pushing active state to its history of states.
+   * @return new cell with active state. 
    */
-  def isPredictive() : Boolean = (m_state & Cell.Predictive) != 0
+  def makeActive : Cell = new Cell(m_steps, true +: m_stateHistory.init, m_distalSegments)
   
   /**
-   * Turns cell into active state.
-   * @return activated cell.
+   * Makes cell inactive by pushing active state to its history of states.
+   * @return new cell with inactive state. 
    */
-  def makeActive() : Cell = new Cell(m_state | Cell.Active)
+  def makeInactive : Cell = new Cell(m_steps, false +: m_stateHistory.init, m_distalSegments)
   
   /**
-   * Turns cell into predictive state.
-   * @return predicted cell.
+   * Checks whether cell was active on a specified step.
+   * @param step (0 - current state).
+   * @return true if cell was active, or false otherwise. 
    */
-  def makePredictive() : Cell = new Cell(m_state | Cell.Predictive)
+  def wasActive(step : Int = 0) : Boolean = m_stateHistory(step)
+  
+  /**
+   * Checks whether cell was active on any step.
+   * @return true if cell was active, or false otherwise. 
+   */
+  def wasEverActive : Boolean = m_stateHistory.contains(true)
+  
+  /**
+   * Adds distal segment to cell.
+   * @param segment the segment which will be added.
+   * @param step prediction step on which segment must be added.
+   * @return cell with new segment.
+   */
+  def addSegment(segment : DistalSegment, step : Int) : Cell = new Cell(m_steps, m_stateHistory,
+      m_distalSegments.updated(step, segment :: m_distalSegments(step)))
+  
+  /**
+   * Checks whether cell is predicted on specified step.
+   * @param cells vector of cells of the region.
+   * @param step on which step we check if cell is predicted.
+   * @return true if cell is predicted, or false otherwise.  
+   */
+  def isPredicted(cells : Vector[Cell], step : Int) : Boolean = {
+    
+    def isSegmentPredicting(s : DistalSegment) : Boolean = {
+      s.overlap(cells, (c : Cell) => if (c.wasActive()) 1 else 0).toFloat /
+          s.numOfConnections.toFloat > Cell.PredictionThreshold
+    }
+    
+    m_distalSegments(step).exists(isSegmentPredicting(_))
+  }
+  
+  /**
+   * Checks whether cell is predicted on any step.
+   * @param cells vector of cells of the region.
+   * @return true if cell is predicted, or false otherwise.  
+   */
+  def isEverPredicted(cells : Vector[Cell]) : Boolean = {
+    val stepsRange = 0 until m_steps
+    stepsRange.exists(isPredicted(cells, _))
+  }
 }
 
 /**
  * @brief object Cell.
  */
 object Cell {
-  private val Active = 0x1;
-  private val Predictive = 0x2;
+  
+  val PredictionThreshold = 0.8F
 }
