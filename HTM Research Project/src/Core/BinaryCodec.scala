2127a176025094558203b737217ff017db8937a7
Maryan Pritsak <maryan.pricak@gmail.com>
2014-11-18 01:30:31 +0200

    Changed interface so that child classes have to implement only encoding and decoding of separate symbols.

--- HTM Research Project/src/Core/BinaryCodec.scala
+++ HTM Research Project/src/Core/BinaryCodec.scala
@@ -9,15 +9,35 @@ abstract class BinaryCodec[T] {
   
   /**
    * Encodes input data into byte vector.
-   * @param data list of input symbols.  
+   * @param data list of input symbols.
    * @return byte vector with encoded data.
    */
-  def encode(data : List[T]) : Vector[Byte]
+  def encode(data : List[T]) : Vector[Byte] = data.map(encodeSymbol(_)).reduce(_ ++ _)
   
   /**
    * Decodes byte vector.
    * @param coode byte vector with encoded data.
    * @return decoded data.
    */
-  def decode(code : Vector[Byte]) : List[T]
+  def decode(code : Vector[Byte]) : List[T] =
+    if (code.isEmpty)
+      Nil
+    else {
+      val (symbol, rest) = decodeSymbol(code)
+      symbol :: decode(rest)
+    }
+  
+  /**
+   * Encodes one symbol into byte vector.
+   * @param symbol input symbol to be encoded.
+   * @return byte vector with encoded data.
+   */
+  protected def encodeSymbol(symbol : T) : Vector[Byte]
+  
+  /**
+   * Decodes byte vector.
+   * @param coode byte vector with encoded data.
+   * @return decoded data.
+   */
+  protected def decodeSymbol(code: Vector[Byte]) : (T, Vector[Byte])
 }
