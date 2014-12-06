d64f8ec97ceae225ef17e1322a066e48195b784b
Maryan Pritsak <maryan.pricak@gmail.com>
2014-12-06 12:13:48 +0200

    Added comments to LowerRegion.

--- HTM Research Project/src/Core/LowerRegion.scala
+++ HTM Research Project/src/Core/LowerRegion.scala
@@ -1,13 +1,28 @@
 package Core
 
+/**
+ * @brief class LowerRegion a partial case of region that receives raw data.
+ */
 class LowerRegion(val deltaEncoder : DeltaEncoder, colMapper : ColumnCellMapper) extends Region(colMapper) {
   
+  /**
+   * converts raw data to byte vector and feeds it to region.
+   * @param data raw data in String representation.
+   * @param activationPercentage how many columns will be activated (0.0 - 1.0 value).
+   * @param learnOn indicates whether region is learning new connections.
+   * @return new Region with remembered history and updated connections if
+   * learning is turned on.
+   */
   def feedRawData(data : List[String], activationPercentage : Float, learnOn : Boolean) : LowerRegion = {
     val (code, encoder) = deltaEncoder.delta(data)
     
     new LowerRegion(encoder, feedData(code, activationPercentage, learnOn).colMapper)
   }
-  
+
+  /**
+   * Makes prediction to all steps.
+   * @return list of predictions.
+   */
   def predictedRawData() : List[List[String]] = {
     def go(step : Int, encoder : DeltaEncoder) : List[List[String]] =
       if (step == colMapper.numOfSteps) Nil
@@ -17,10 +32,10 @@ class LowerRegion(val deltaEncoder : DeltaEncoder, colMapper : ColumnCellMapper)
         
         raw :: go(step + 1, newEncoder)
       }
-    
+
     go(1, deltaEncoder)
   }
-  
+
   private def intListToVector(list : List[Int]) : Vector[Byte] = list match {
     case Nil => Vector.fill(colMapper.numOfCells)(0.toByte)
     case l :: ls => intListToVector(ls).updated(l, 1.toByte)
